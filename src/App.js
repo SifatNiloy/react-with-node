e0ed263fbd8b30b7be95e465c2d9a10cb88088aa
unknown <sifatniloy18@gmail.com>
2022-09-19 20:09:46 -0600

    fetch post data

--- src/App.js
+++ src/App.js
@@ -8,10 +8,38 @@ function App() {
     fetch('http://localhost:5000/users')
       .then(res => res.json())
       .then(data => setUsers(data))
+
+
   }, [])
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    fetch('http://localhost:5000/users', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+  }
   return (
     <div className="App">
       <h1>My own data:{users.length} </h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" placeholder='name' required />
+        <input type="text" name="email" id="" placeholder='email' required />
+        <input type="submit" value="add user" />
+      </form>
       <ul>
         {
           users.map(user => <li key={user.id}>Name: {user.name} Email: {user.email}</li>)
